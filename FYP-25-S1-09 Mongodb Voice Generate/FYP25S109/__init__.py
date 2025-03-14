eeed07f31181cd17a7d8004f236f67d270ae121a
zcx73100 <166374071+zcx73100@users.noreply.github.com>
2025-03-14 19:30:24 +0800

    Add files via upload

---
+++ FYP-25-S1-09 Mongodb Voice Generate/FYP25S109/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+import sys
+import os
+mongo = PyMongo()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Flask Configuration
+    app.config['SECRET_KEY'] = 'fyp25'
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/fyps12509"
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+    # Initialize MongoDB
+    mongo.init_app(app)
+
+    # Register Blueprints
+    from .boundary import boundary
+    app.register_blueprint(boundary, url_prefix='/')
+
+    return app
